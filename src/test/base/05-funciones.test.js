59cc3c35f3b754801d5310ac6e0b211aacefda88
abdiel_249 <abdielorellana3@gmail.com>
2020-12-06 15:01:54 -0400

    05-functions test js

--- src/test/base/05-funciones.test.js
+++ src/test/base/05-funciones.test.js
@@ -1,10 +1,10 @@
-const { getUser } = require("../../base/05-funciones");
+const { getUser, getUsuarioActivo} = require("../../base/05-funciones");
 
 describe( "pruebas en 05-funciones", () => {
   
   test('getUser debe devolver un objeto', () => {
     const userTest = {
-      uid: 1235,
+      uid: 'ABC123',
       username: 'El_Papi1502'
     }
 
@@ -13,4 +13,18 @@ describe( "pruebas en 05-funciones", () => {
     expect ( user ).toEqual( userTest );
   });
 
+  // getUsuarioActivo(_nombre_) debe devolver un objeto con el nombre
+
+  test('getUsuarioActivo(_nombre_) debe devolver un objeto con el nombre', () => {
+    const nombre = "job";
+    // const userTest = 
+    const user = getUsuarioActivo(nombre);
+
+    expect( user ).toEqual( {
+      uid: 'ABC567',
+      username: nombre
+    } );
+  })
+  
+  
 })
