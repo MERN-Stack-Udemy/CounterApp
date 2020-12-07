fa99a5f7893f6ddb03f38f369b847549d78f96d0
abdiel_249 <abdielorellana3@gmail.com>
2020-12-07 18:51:33 -0400

    08-importacion-exportacione test - getHeroesID() test

--- src/test/base/08-imp-exp.test.js
+++ src/test/base/08-imp-exp.test.js
@@ -1,4 +1,4 @@
-import  { getHeroeById } from "../../base/08-imp-exp";
+import  { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
 import  heroes from "../../data/Heroes";
 
 describe('08-imp-exp.test funciones de Heroes ', () => {
@@ -23,7 +23,7 @@ describe('08-imp-exp.test funciones de Heroes ', () => {
     
     expect( hero ).toEqual( heroData );
   })
-  
+
   test('debe devolver Undefined si el ID NO existe ', () => {
     
     const ID = 56;
@@ -31,5 +31,36 @@ describe('08-imp-exp.test funciones de Heroes ', () => {
     
     expect( hero ).toBe( undefined );
   })
-  
+   // Marvel - DC
+  test('deve devolver un areglo con los heroes de Marvel', () => {
+    const owner = "Marvel";
+    const owners = getHeroesByOwner(owner);
+    // console.log(owners)
+    const ownerData = heroes.filter( hero => hero.owner === owner );
+    
+    expect( owners ).toEqual( ownerData );
+  })
+
+  test('deve devolver un areglo de tamanio 2 con los heroes de Marvel', () => {
+    const owner = "Marvel";
+    const owners = getHeroesByOwner(owner);
+    const lenght = 2
+    expect( owners.length ).toBe( lenght );
+  })
+
+  test('deve devolver un areglo con los heroes de DC', () => {
+    const owner = "DC";
+    const owners = getHeroesByOwner(owner);
+    // console.log(owners)
+    const ownerData = heroes.filter( hero => hero.owner === owner );
+    
+    expect( owners ).toEqual( ownerData );
+  })
+  test('deve devolver un areglo de tamanio 3 con los heroes de DC', () => {
+    const owner = "DC";
+    const owners = getHeroesByOwner(owner);
+    const lenght = 3;
+    expect( owners.length ).toBe( lenght );
+  })
+
 })
